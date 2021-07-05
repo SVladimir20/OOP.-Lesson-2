cb5faf92f435d7846b6b90f58ce4da1aaa51baa2
Sbitnev <sbitnev.vladimir1@gmail.com>
2021-07-05 20:03:02 +0300

    Некоторые исправления

--- 1.cpp
+++ 1.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 using namespace std;
-size_t counter{};
-enum class ID
-{
-	id_1 = 1,
-	id_2 = 2,
-	id_3 = 3,
-	id_4 = 4,
-	id_5 = 5,
-};
+size_t id{};
 class Person
 {
 public:
-	Person(ID id, string n, unsigned char a, string s, unsigned char w) : name(n), age(a), sex(s), weight(w) {}
+	Person(string n, unsigned char a, string s, unsigned char w) : name(n), age(a), sex(s), weight(w)
+	{
+		id++;
+	}
 	void setName(string n)
 	{
 		name = n;
@@ -55,8 +50,9 @@ protected:
 class Student : public Person
 {
 public:
-	Student(ID id, string n, unsigned char a, string s, unsigned char w, unsigned int y) : Person(id, n, a, s, w), year(y)
+	Student(string n, unsigned char a, string s, unsigned char w, unsigned int y) : Person(n, a, s, w), year(y)
 	{
+		static size_t counter{};
 		counter++;
 	}
 	void setYear(unsigned int y)
@@ -76,27 +72,27 @@ void Print(int choose, Student& s1, Student& s2, Student& s3, Student& s4, Stude
 	switch (choose)
 	{
 	case 1:
-		cout << endl << "ID: " << static_cast<int>(ID::id_1) << ", имя: " << s1.getName() << ", возраст: "
+		cout << endl << "ID: " << id << ", имя: " << s1.getName() << ", возраст: "
 			<< static_cast<int>(s1.getAge()) << ", пол: " << s1.getSex() << ", вес: "
 			<< static_cast<int>(s1.getWeight()) << ", год обучения: " << s1.getYear() << ".";
 		break;
 	case 2:
-		cout << endl << "ID: " << static_cast<int>(ID::id_2) << ", имя: " << s2.getName() << ", возраст: "
+		cout << endl << "ID: " << id << ", имя: " << s2.getName() << ", возраст: "
 			<< static_cast<int>(s2.getAge()) << ", пол: " << s2.getSex() << ", вес: "
 			<< static_cast<int>(s2.getWeight()) << ", год обучения: " << s2.getYear() << ".";
 		break;
 	case 3:
-		cout << endl << "ID: " << static_cast<int>(ID::id_3) << ", имя: " << s3.getName() << ", возраст: "
+		cout << endl << "ID: " << id << ", имя: " << s3.getName() << ", возраст: "
 			<< static_cast<int>(s3.getAge()) << ", пол: " << s3.getSex() << ", вес: "
 			<< static_cast<int>(s3.getWeight()) << ", год обучения: " << s3.getYear() << ".";
 		break;
 	case 4:
-		cout << endl << "ID: " << static_cast<int>(ID::id_4) << ", имя: " << s4.getName() << ", возраст: "
+		cout << endl << "ID: " << id << ", имя: " << s4.getName() << ", возраст: "
 			<< static_cast<int>(s4.getAge()) << ", пол: " << s4.getSex() << ", вес: "
 			<< static_cast<int>(s4.getWeight()) << ", год обучения: " << s4.getYear() << ".";
 		break;
 	case 5:
-		cout << endl << "ID: " << static_cast<int>(ID::id_5) << ", имя: " << s5.getName() << ", возраст: "
+		cout << endl << "ID: " << id << ", имя: " << s5.getName() << ", возраст: "
 			<< static_cast<int>(s5.getAge()) << ", пол: " << s5.getSex() << ", вес: "
 			<< static_cast<int>(s5.getWeight()) << ", год обучения: " << s5.getYear() << ".";
 		break;
@@ -106,11 +102,11 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int choose;
-	Student s1(ID::id_1, "Первак", 19, "Мужчина", 80, 2020);
-	Student s2(ID::id_2, "Вторина", 18, "Женщина", 65, 2021);
-	Student s3(ID::id_3, "Третякович", 23, "Мужчина", 100, 2017);
-	Student s4(ID::id_4, "Четвертухина", 23, "Женщина", 70, 2017);
-	Student s5(ID::id_5, "Пятихатченко", 24, "Мужчина", 90, 2016);
+	Student s1("Первак", 19, "Мужчина", 80, 2020);
+	Student s2("Вторина", 18, "Женщина", 65, 2021);
+	Student s3("Третякович", 23, "Мужчина", 100, 2017);
+	Student s4("Четвертухина", 23, "Женщина", 70, 2017);
+	Student s5("Пятихатченко", 24, "Мужчина", 90, 2016);
 	cout << "Введите ID студента (от 1 до 5): ";
 	cin >> choose;
 	Print(choose, s1, s2, s3, s4, s5);
